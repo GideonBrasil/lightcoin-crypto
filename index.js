f48a9cd26b66ad25dcfc9b4bd77b0aea76eb956b
GideonBrasil <gidxx_z@yahoo.com>
2019-03-06 14:02:31 +0000

    modify driver code

--- index.js
+++ index.js
@@ -40,18 +40,24 @@ class Deposit extends Transaction {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-const myAccount = new Account('snow-patrol');
+const myAccount = new Account('Poor Labber');
 
-t1 = new Withdrawal(50.25, myAccount);
+console.log(`'${myAccount.username}'s account has a balance of ${myAccount.balance} dollars.`);
+
+t1 = new Deposit(500.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+console.log('Salary deposit: +', t1.amount, 'dollars.');
 
-t2 = new Withdrawal(9.99, myAccount);
+t2 = new Withdrawal(50.25, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Pay phone bill: -', t2.amount, 'dollars.');
 
-t3 = new Deposit(120.00, myAccount)
+t3 = new Withdrawal(9.99, myAccount);
 t3.commit();
-console.log('Transaction 3:', t3);
+console.log('Pay for lunch at DQ: -', t3.amount, 'dollars.');
 
-console.log(`'${myAccount.username}'s account has a balance of ${myAccount.balance}`);
+t4 = new Deposit(120.00, myAccount)
+t4.commit();
+console.log('Side job deposit: +', t4.amount, 'dollars.');
+
+console.log(`'${myAccount.username}'s account has a balance of ${myAccount.balance} dollars.`);
